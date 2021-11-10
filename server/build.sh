9b676a99c8e06c2d63d12d3ca6135198a7c644fe
Alexey Pleshakov <alexey.pleshakov@huawei.com>
2021-11-10 12:13:47 +0300

    enabled parallel build

--- server/build.sh
+++ server/build.sh
@@ -1,7 +1,7 @@
 mkdir -p build
 cd build
 $UTBOT_CMAKE_BINARY -G "Ninja" -DCMAKE_INSTALL_PREFIX=$UTBOT_ALL/server-install ..
-$UTBOT_CMAKE_BINARY --build .
+$UTBOT_CMAKE_BINARY --build . --parallel `nproc`
 $UTBOT_CMAKE_BINARY --install .
 
 ./UTBot_UnitTests info
