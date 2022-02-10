5de7b6def3c2371018682b887835593f0d9daff4
Samat Gaynutdinov <gainutdinov.sm@phystech.edu>
2022-02-11 01:38:23 +0300

    fix: show server logs & test logs for client

--- server/src/Server.h
+++ server/src/Server.h
@@ -259,6 +259,9 @@ private:
         const string client;
     };
 
+    //recommended size from loguru
+    const static size_t LOGURU_BUFFER_SIZE = 17;
+
     static void logToClient(void *channel, const loguru::Message &message);
 
     static void gtestLog(void *channel, const loguru::Message &message);
