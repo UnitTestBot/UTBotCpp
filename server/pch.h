73f1336fbe4df21e38da260b63d6c4ad42f9e878
Pavel <operasfantom@gmail.com>
2022-03-10 02:51:47 +0400

    Add precompiled header

---
+++ server/pch.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2012-2021. All rights reserved.
+ */
+
+#pragma once
+
+// stl
+#include <algorithm>
+#include <chrono>
+#include <condition_variable>
+#include <exception>
+#include <experimental/type_traits>
+#include <filesystem>
+#include <forward_list>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <iterator>
+#include <list>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <unistd.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <variant>
+#include <vector>
+
+// grpc
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/server_context.h>
+#include <protobuf/testgen.grpc.pb.h>
+#include <protobuf/testgen.pb.h>
+#include <protobuf/util.pb.h>
+
+// loguru
+#include "loguru.h"
+
+// json
+#include "json.hpp"
+
+// tsl
+#include <tsl/ordered_map.h>
+#include <tsl/ordered_set.h>
