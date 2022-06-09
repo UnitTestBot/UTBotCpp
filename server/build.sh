d37a813c4c682116fc1fb66b002f3fcf635797fd
Vladislav Kalugin <kalugin.vladislav@huawei.com>
2022-06-09 18:07:54 +0300

    Add version to github actions

--- server/build.sh
+++ server/build.sh
@@ -2,6 +2,15 @@
 
 mkdir -p build
 cd build
-$UTBOT_CMAKE_BINARY -G "Ninja" -DCMAKE_INSTALL_PREFIX=$UTBOT_ALL/server-install ..
+
+if [ -z $PROJECT_VERSION ]; then
+  PROJECT_VERSION="0.0.0"
+fi
+
+if [ -z $RUN_INFO ]; then
+  RUN_INFO="local"
+fi
+
+$UTBOT_CMAKE_BINARY -G "Ninja" -DCMAKE_INSTALL_PREFIX=$UTBOT_ALL/server-install -DPROJECT_VERSION=$PROJECT_VERSION -DRUN_INFO=$RUN_INFO ..
 $UTBOT_CMAKE_BINARY --build .
 $UTBOT_CMAKE_BINARY --install .
