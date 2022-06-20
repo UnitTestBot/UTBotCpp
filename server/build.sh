06b689e0606dc0c4cb4defa023ec2c385ac78c0e
Vladislav Kalugin <kalugin.vladislav@huawei.com>
2022-06-20 18:20:36 +0300

    Unify server and vscode plugin version

--- server/build.sh
+++ server/build.sh
@@ -3,14 +3,6 @@
 mkdir -p build
 cd build
 
-if [ -z $PROJECT_VERSION ]; then
-  PROJECT_VERSION="0.0.0"
-fi
-
-if [ -z $RUN_INFO ]; then
-  RUN_INFO="local"
-fi
-
-$UTBOT_CMAKE_BINARY -G "Ninja" -DCMAKE_INSTALL_PREFIX=$UTBOT_ALL/server-install -DPROJECT_VERSION=$PROJECT_VERSION -DRUN_INFO=$RUN_INFO ..
+$UTBOT_CMAKE_BINARY -G "Ninja" -DCMAKE_INSTALL_PREFIX=$UTBOT_ALL/server-install ..
 $UTBOT_CMAKE_BINARY --build .
 $UTBOT_CMAKE_BINARY --install .
