2171673a075f33e6385a61d365adcf9b41531cb7
Vladislav Kalugin <kalugin.vladislav@huawei.com>
2022-07-01 11:41:43 +0300

    version unification for docker, server and plugin

--- docker/release_distribution_scripts/utbot_run_system.sh
+++ docker/release_distribution_scripts/utbot_run_system.sh
@@ -117,13 +117,8 @@ then
 
   log "Starting a new server process; logs are written into [$UTBOT_LOGS_FOLDER] folder"
   start_process "$UTBOT_PROCESS_PATTERN" "$UTBOT_EXECUTABLE_PATH" "$UTBOT_SERVER_OPTIONS" "$UTBOT_STDOUT_LOG_FILE" "$UTBOT_PID_FILE"
-  repeats=0
-  while [ $repeats -le 20 ] && [ $(wc -l < $UTBOT_STDOUT_LOG_FILE) -lt 6 ]
-  do
-    sleep 0.1
-    repeats=$(( repeats + 1 ))
-  done
-  head $UTBOT_STDOUT_LOG_FILE -n 6
+  sleep 5
+  head $UTBOT_STDOUT_LOG_FILE -n 15
 fi
 
 if [ "$1" = "cli" ]
