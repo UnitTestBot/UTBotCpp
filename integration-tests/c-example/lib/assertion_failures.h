0a32aa75f1612094277706c66877f6e0e8b3c3b7
belous-dp <belous.dp@gmail.com>
2022-08-05 17:52:28 +0300

    supported examples added (#339)
    added supported examples to c-example

--- integration-tests/c-example/lib/assertion_failures.h
+++ integration-tests/c-example/lib/assertion_failures.h
@@ -4,7 +4,9 @@
 #include "assert.h"
 
 int buggy_function1(int a, int b);
+
 int buggy_function2(int a);
+
 int buggy_function3(int a, int b);
 
-#endif //UNITTESTBOT_SRC_ASSERT_FAIL_H
+#endif //SIMPLE_TEST_PROJECT_SRC_ASSERT_FAIL_H
