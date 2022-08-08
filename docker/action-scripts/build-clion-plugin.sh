13006f961f613f708609d51a6c4cc0735a85f9bd
Arseniy Volynets <vol0ncar@yandex.ru>
2022-08-08 13:19:23 +0300

    Use actions/setup-java for setting up jdk

--- docker/action-scripts/build-clion-plugin.sh
+++ docker/action-scripts/build-clion-plugin.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-#install java zulu 11 distribution
-sudo apt update -y
-sudo apt install dirmngr --install-recommends -y
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
-sudo apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main' -y
-
-sudo apt update -y
-sudo apt install zulu-11 -y
-
-
 set -e
+
 cd clion-plugin
 ./gradlew assemble
