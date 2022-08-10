64b0daf408db5aa149d9756e56efe6b7aeadbfd9
Alexey Utkin <alexey.utkin@gmail.com>
2022-08-10 13:52:25 +0300

    [BUG] Cannot parse coverage.json as it contains warnings #345 (#380)
    - parse leading lines as warning with logging those lines

--- server/src/utils/JsonUtils.cpp
+++ server/src/utils/JsonUtils.cpp
@@ -1,26 +1,53 @@
 #include "JsonUtils.h"
-
+#include "StringUtils.h"
 #include "utils/FileSystemUtils.h"
+#include "utils/path/FileSystemPath.h"
 
 #include "loguru.h"
 
-#include "utils/path/FileSystemPath.h"
-#include <fstream>
 #include <exception>
+#include <fstream>
 
 namespace JsonUtils {
     nlohmann::json getJsonFromFile(const fs::path &path) {
         std::ifstream stream(path.string());
         std::stringstream buffer;
         buffer << stream.rdbuf();
+        std::string contentToParse = buffer.str();
         try {
-            nlohmann::json coverageJson = nlohmann::json::parse(buffer.str());
+            // skip warning header lines from input stream like
+            //    warning: 1 functions have mismatched data
+            //    {JSON}
+            std::string::size_type pos = 0;
+            while (pos < contentToParse.size()) {
+                const char ch = contentToParse[pos];
+                if (!std::isspace(ch)) {
+                    if (ch == '{' || ch == '[') {
+                        break;
+                    }
+                    pos = contentToParse.find('\n', pos);
+                    if (pos == std::string::npos) {
+                        const std::invalid_argument e("empty json file");
+                        LOG_S(ERROR) << e.what();
+                        throw e;
+                    }
+                }
+                ++pos;
+            }
+            if (pos > 0) {
+                std::string warningHeader = contentToParse.substr(0, pos);
+                StringUtils::trim(warningHeader);
+                if (!warningHeader.empty()) {
+                    LOG_S(WARNING) << "JSON header: [" << warningHeader << "] in: " << path.string();
+                }
+            }
+            nlohmann::json coverageJson = nlohmann::json::parse(contentToParse.substr(pos));
             return coverageJson;
         } catch (const std::exception &e) {
-            LOG_S(ERROR) << e.what() << ": " << buffer.str() << " in: " << path.string();
+            LOG_S(ERROR) << e.what() << ": " << contentToParse << " in: " << path.string();
             throw e;
         } catch (...) {
-            LOG_S(ERROR) << buffer.str();
+            LOG_S(ERROR) << "Crash: " << contentToParse << " in: " << path.string();
             throw;
         }
     }
