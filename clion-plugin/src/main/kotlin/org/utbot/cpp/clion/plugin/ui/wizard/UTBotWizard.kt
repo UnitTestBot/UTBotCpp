2a09798a2ea1877793fad9775d3655f24450239d
Arseniy Volynets <vol0ncar@yandex.ru>
2022-08-19 16:45:01 +0300

    fix: unfinished wizard modifies settings

--- clion-plugin/src/main/kotlin/org/utbot/cpp/clion/plugin/ui/wizard/UTBotWizard.kt
+++ clion-plugin/src/main/kotlin/org/utbot/cpp/clion/plugin/ui/wizard/UTBotWizard.kt
@@ -17,7 +17,7 @@ import java.awt.event.KeyEvent
 
 class UTBotWizard(private val project: Project) : AbstractWizard<UTBotBaseWizardStep>("UTBot: Quickstart", project) {
     // copy of settings to make changes during wizard steps
-    private val mySettingsModel = UTBotSettingsModel(project.settings.storedSettings, projectIndependentSettings)
+    private val mySettingsModel = UTBotSettingsModel(project.settings.storedSettings.copy(), projectIndependentSettings.copy())
 
     init {
         addStep(IntroStep())
