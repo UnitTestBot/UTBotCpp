12039ff06cb3c7e9097e8f96f82ec3a24ee73032
Arseniy Volynets <vol0ncar@yandex.ru>
2022-09-06 17:24:21 +0300

    fix

--- vscode-plugin/src/test/run.sh
+++ vscode-plugin/src/test/run.sh
@@ -14,8 +14,6 @@ echo "VSCODE_VERSION_DIR=$VSCODE_VERSION_DIR"
 export DISPLAY=':99.0'
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
-$PROJECT_DIR/server/build/utbot server > /dev/null 2>&1 &
-
 #Executing the test suite
 #TODO: fetch workspace folders automatically from .vscode/launch.json
 $VSCODE_VERSION_DIR/VSCode-linux-x64/code $PROJECT_DIR/integration-tests/c-example-mini \
