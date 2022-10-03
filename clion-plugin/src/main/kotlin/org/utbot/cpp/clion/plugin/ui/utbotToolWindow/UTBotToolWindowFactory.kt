b454495065329b60283588e2cf79dc8caec37a4e
Arseniy Volynets <vol0ncar@yandex.ru>
2022-10-03 13:28:08 +0300

    refresh targets after tool window is open

--- clion-plugin/src/main/kotlin/org/utbot/cpp/clion/plugin/ui/utbotToolWindow/UTBotToolWindowFactory.kt
+++ clion-plugin/src/main/kotlin/org/utbot/cpp/clion/plugin/ui/utbotToolWindow/UTBotToolWindowFactory.kt
@@ -13,15 +13,20 @@ import org.utbot.cpp.clion.plugin.ui.utbotToolWindow.targetToolWindow.UTBotTarge
 class UTBotToolWindowFactory : ToolWindowFactory, DumbAware {
     private val logger = Logger.getInstance(this::class.java)
 
+    // tool window is created lazily: if user opens it
+    // => in tests tool window is not created
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         logger.info("createToolWindowContent was called")
         val contentManager = toolWindow.contentManager
+        val targetsController = project.service<UTBotTargetsController>()
         val targetsToolWindow = contentManager.factory.createContent(
-            project.service<UTBotTargetsController>().targetsToolWindow, UTBot.message("toolwindow.targets.displayName"), false
+            targetsController.targetsToolWindow, UTBot.message("toolwindow.targets.displayName"), false
         )
         val logsToolWindow =
             contentManager.factory.createContent(ConsoleToolWindow(project), UTBot.message("toolwindow.logs.displayName"), false)
         toolWindow.contentManager.addContent(logsToolWindow)
         toolWindow.contentManager.addContent(targetsToolWindow)
+        // when user opens tool window refresh targets
+        targetsController.requestTargetsFromServer()
     }
 }
