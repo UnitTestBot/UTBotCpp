3caa4c38518f89624772712f00c642d59b65a63d
Vladislav Kalugin <kalugin.vlad@gmail.com>
2024-03-12 12:04:19 +0300

    Add delete all submodules

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pwd=$PWD
 chmod +x $pwd/submodules/build-klee.sh $pwd/submodules/Bear/build.sh $pwd/server/build.sh
 cd $pwd/submodules && ./build-klee.sh
 cd $pwd/submodules/Bear && ./build.sh
-if [ "$1" -eq "gh" ]; then
+if [ "$1" = "gh" ]; then
   rm -rf submodules
 fi
 cd $pwd/server && ./build.sh
